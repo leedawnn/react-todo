5f9aa8491e0a34aa20661b6f9d92b5764ae26b9a
leedawnn <2g_hee@naver.com>
2021-12-18 02:20:37 +0900

    :sparkles: feat: 할 일을 입력할 수 있는 input창과 추가 버튼 생성
    useState를 사용하여 input value에 연결시켜 입력하는 값을 렌더링 되게함. 추가 버튼을 눌렀을 때 새로고침 되는 이벤트가 발생되지 않게 했고, 입력 값이 비어있으면 콜백 함수가 그대로 종료되게함. 입력값이 전송되면 input창 초기화.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const onChange = event => setTodo(event.target.value);
+  const onSubmit = event => {
+    event.preventDefault();
+    if (todo === '') {
+      return;
+    }
+    setTodo('');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type='text'
+          placeholder='할 일을 적어보세요.'
+        />
+        <button>추가</button>
+      </form>
     </div>
   );
 }
